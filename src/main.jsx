66d97aa0ccb0b3d8c96252e7ead1151b72df19cc
Ersin Ali <alyersin@yahoo.com>
2024-05-20 01:37:33 +0300

    20may

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={"/<REPO>/"}>
+  <BrowserRouter basename={"/movie-database/"}>
     <App />
   </BrowserRouter>
 );
